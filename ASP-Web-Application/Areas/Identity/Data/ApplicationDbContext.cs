2549fb94e291acf25994588971d34e233086e067
Walkername <Walkername@DESKTOP-LFAJ1H1>
2023-03-21 02:44:40 +0300

    Добавьте файлы проекта.

---
+++ ASP-Web-Application/Areas/Identity/Data/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using ASP_Web_Application.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Build.Framework;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ASP_Web_Application.Areas.Identity.Data;
+
+public class ApplicationDbContext : IdentityDbContext<AppUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+
+        builder.ApplyConfiguration(new AppUserEntityConfiguration());
+    }
+}
+
+public class AppUserEntityConfiguration : IEntityTypeConfiguration<AppUser>
+{
+    public void Configure(EntityTypeBuilder<AppUser> builder)
+    {
+        builder.Property(u => u.FirstName).HasMaxLength(20);
+        builder.Property(u => u.LastName).HasMaxLength(20);
+    }
+}
